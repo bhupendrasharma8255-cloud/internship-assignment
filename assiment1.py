be11d9de4beff7ea297a83c6df369653fe45c0d1
bhupendra sharma <bhupendrasharma8255@gmail.com>
2026-06-19 12:45:55 +0530

    Add files via upload

---
+++ assiment1.py
@@ -0,0 +1,104 @@
+Assignment: 1 
+# 1. Explain the difference between the following data types with examples: 
+
+# • Integer (int) : these are the whole numbers without the decimal
+# ex: 15,-6
+# # • Float (float) : these are the numbers with decimal
+# ex: 45.6,2.13
+# # • String (str)  : these are the sequence of character enclosed in quotes
+# ex: "hello"
+# # • Boolean (bool): these represent true or false values
+# ex: true,false
+
+
+# 2. Write a Python program to create three variables: 
+# • name  
+# • age  
+# • city  
+# Store your details in them and print the values.
+
+# name = "bhupendra"
+# age = "20"
+# city = "pilani"
+# print("name",name)
+# print("city",city)
+# print("age",age)
+
+ 
+# 3. Write a Python program that: 
+# Takes a user's name as input.  
+# Prints the name in uppercase.    
+# Prints the total number of characters in the name. 
+
+# name = input("enter your name")
+# print("upper_case:",name.upper())
+# print("total character:",len(name))
+
+
+# 4. Explain any five commonly used string methods in Python with examples. 
+
+# 1. upper
+# text = "hello"
+# print(text.upper())
+# 2. lower
+# text = "HELLO"
+# print(text.lower())
+# 3. replace
+# text = "python"
+# print(text.replace("python","java"))
+# 4. strip
+# text = " python "
+# print(text.strip())
+# 5. split
+# text = "ram,shyam,bhupendra"
+
+
+# 5. Create a list containing the names of five fruits. 
+# • Print the complete list.  
+# • Print the first and last element.  
+# • Print the total number of items in the list. 
+
+# text = ["apple","banana","mango","orange","grapes"]
+# print("compelete list:",text)
+# print("first element:",text[0])
+# print("last element:",text[-1])
+# print("total element:",len(text))
+
+
+# 6. Write a Python program to: 
+# • Create a list of numbers [10, 20, 30, 40, 50]  
+# • Add 60 to the list.  
+# • Remove 20 from the list.  
+# • Print the updated list.
+
+# number = [10,20,30,40,50]
+# number.append(60)
+# number.remove(20)
+# print("updated list:",number)
+
+
+# 7. What is Artificial Intelligence (AI)? Explain its importance and mention 
+# AI is a branch of computer science that enables machine to perform task as the human intelligence and make
+# work easier.
+
+# # any four real-life applications of AI. 
+# 1.self driving cars
+# 2.virtual assistance
+# 3.healthcare systems 
+# 4.chatbox
+
+
+# 8.  Identify whether the following are examples of AI and explain why: 
+# • ChatGPT :yes,it is ai
+# uses machine learning and natural language processing to understand and generate human like text
+  
+# • Google Maps route prediction : yes,it uses ai 
+# uses ai to predict traffic pattern
+
+# • Calculator : no, it is not ai
+# perform fixed mathematical operators acc. to predefined rules
+ 
+# • Netflix recommendations : yes , it uses ai
+#   uses ai algorithms to recommend content based on viewing history and preferences
+# • Voice assistants (Alexa/Siri): yes, it is ai 
+# use speech recognition and natural language processing to understand and respond to voice commands
---
+++ assiment2.py
@@ -0,0 +1,89 @@
+#   Create a class named Student with attributes: 
+# o name  
+# o age  
+# o course  
+#                 Create an object and print all the details. 
+
+
+ 
+# 2. Create a class named Car with attributes: 
+# o brand  
+# o model  
+# o price  
+#              Create two objects and display their details. 
+ 
+# 3. Create a class named Employee and use a constructor (__init__) to 
+# initialize: 
+# o employee_id  
+# o name  
+# o salary  
+#             Print the employee details. 
+ 
+# 4. Create a class named Rectangle with attributes: 
+# o length  
+# o width  
+#            Create a method to calculate and print the area of the rectangle. 
+ 
+# 5. Create a class named Circle with an attribute radius. 
+#             Create a method that calculates and prints the area of the circle. 
+ 
+# 6. Create a class named BankAccount with: 
+# o account_holder  
+# o balance 
+#  Create methods: 
+# o deposit(amount)  
+# o withdraw(amount)  
+#             Display the updated balance after each transaction. 
+ 
+# 7. Create a class named Animal with a method sound(). 
+#             Create a child class Dog that overrides the sound() method and prints         
+# "Bark". 
+ 
+# 8. Create a class named Person with attributes: 
+# o name  
+# o age  
+#           Create a child class Student with an additional attribute: 
+# o roll_number  
+#           Display all the details using an object of the Student class. 
+ 
+# 9. Create a class named Calculator with methods: 
+# o add()  
+# o subtract()  
+# o multiply()  
+# o divide()  
+#          Take two numbers as input and perform all operations. 
+ 
+# 10. Create a class named LibraryBook with attributes:  
+# ● book_name  
+# ● author  
+# ● price  
+#             Create a method display_book_info() that prints all book details. Create    
+# at least three book objects and display their information.  Create methods: 
+# o deposit(amount)  
+# o withdraw(amount)  
+#             Display the updated balance after each transaction. 
+ 
+# 7. Create a class named Animal with a method sound(). 
+#             Create a child class Dog that overrides the sound() method and prints         
+# "Bark". 
+ 
+# 8. Create a class named Person with attributes: 
+# o name  
+# o age  
+#           Create a child class Student with an additional attribute: 
+# o roll_number  
+#           Display all the details using an object of the Student class. 
+ 
+# 9. Create a class named Calculator with methods: 
+# o add()  
+# o subtract()  
+# o multiply()  
+# o divide()  
+#          Take two numbers as input and perform all operations. 
+ 
+# 10. Create a class named LibraryBook with attributes:  
+# ● book_name  
+# ● author  
+# ● price  
+#             Create a method display_book_info() that prints all book details. Create    
+# at least three book objects and display their information. 
